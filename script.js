3909d68960aefa0bb25ef506b6abf1c41035a9e1
Ankit Pandit <58819767+ankit1215@users.noreply.github.com>
2025-12-05 15:32:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+  if (arr.length <= 1) return 0;
+
+  arr.sort((a, b) => a - b);
+
+  let cost = 0;
+
+  while (arr.length > 1) {
+   
+    let a = arr.shift();
+    let b = arr.shift();
+
+    let sum = a + b;
+    cost += sum;
+
+    let i = 0;
+    while (i < arr.length && arr[i] < sum) i++;
+    arr.splice(i, 0, sum);
+  }
+
+  return cost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
